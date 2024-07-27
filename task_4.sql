d3de605ae10232d416c09190354536af1fc302c4
timon-yego <kipyegotimothy99@gmail.com>
2024-07-27 21:22:56 +0300

    added task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -6,7 +6,7 @@ SELECT
     COLUMN_DEFAULT, 
     EXTRA 
 FROM 
-    information_schema.COLUMNS 
+   INFORMATION_SCHEMA.COLUMNS 
 WHERE 
     TABLE_SCHEMA = 'alx_book_store' 
-    AND TABLE_NAME = 'books';
+    AND TABLE_NAME = 'Books';
