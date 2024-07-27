c6ce0b9e3122e7bd9f3dc1f1342b6e9955cc2be4
timon-yego <kipyegotimothy99@gmail.com>
2024-07-27 20:14:51 +0300

    modified task_2.sql

--- task_2.sql
+++ task_2.sql
@@ -38,6 +38,6 @@ CREATE TABLE IF NOT EXISTS order_details (
     order_id INT,
     book_id INT,
     quantity DOUBLE,
-    FOREIGN KEY (order_id) REFERENCES orders(order_id),
-    FOREIGN KEY (book_id) REFERENCES books(book_id)
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
